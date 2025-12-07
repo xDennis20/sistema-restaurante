e49a9e0efedaf0651515b3cd241a2542ae2f8ac4
xDesple20 <dennis_6@outlook.com>
2025-12-07 16:45:34 -0500

    Probando que los productos se miren en la web

--- SistemaRestaurante/urls.py
+++ SistemaRestaurante/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import menu_restuarante
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', menu_restuarante, name= 'menu')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Producto,Categoria
 # Create your views here.
+
+def menu_restuarante(request):
+    cargar_productos = Producto.objects.filter(disponible=True)
+    cargar_categorias = Categoria.objects.all()
+
+    contexto = {
+        "productos" : cargar_productos,
+        "categorias" : cargar_categorias
+    }
+
+    return render(request, "core/menu.html", contexto)
