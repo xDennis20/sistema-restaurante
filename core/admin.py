5ef961f5c82caa26ea947c2a2c0e7e6b61111924
xDesple20 <dennis_6@outlook.com>
2025-12-08 18:42:18 -0500

    Agregue nuevos modelos: Mesa,Pedido,DetallePedido y cambio de nombre el modelo Producto a Plato para que sea mas entendible

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Producto
+from .models import Categoria, Plato
 # Register your models here.
 admin.site.register(Categoria)
-admin.site.register(Producto)
+admin.site.register(Plato)
--- core/models.py
+++ core/models.py
@@ -1,16 +1,47 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     def __str__(self):
         return self.nombre
 
-class Producto(models.Model):
+class Plato(models.Model):
     nombre = models.CharField(max_length=200)
     precio = models.DecimalField(max_digits=10,decimal_places=2)
-    descripcion = models.TextField(blank=True,null = True, verbose_name="Descripicion")
+    descripcion = models.TextField(blank=True,null=True, verbose_name="Descripicion")
     disponible = models.BooleanField(default=True,verbose_name= "Disponible?")
 
     def __str__(self):
         return f"{self.nombre}: {self.precio}"
+
+class Mesa(models.Model):
+    numero = models.IntegerField(unique=True,verbose_name="Numero de mesa")
+    nombre = models.CharField(max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return f"Mesa {self.numero}"
+
+class Pedido(models.Model):
+    ESTADOS = [
+        ("pendiente","Pendiente"),
+        ("Pagado","Pagado")
+    ]
+
+    mesa = models.ForeignKey(Mesa,on_delete=models.PROTECT)
+    estado = models.CharField(max_length=40, choices= ESTADOS, default="pendiente")
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - Mesa {self.mesa.numero}"
+
+class DetallePedido(models.Model):
+    pedido = models.ForeignKey(Pedido,on_delete=models.CASCADE)
+    cantidad = models.IntegerField(verbose_name="Cantidad de plato")
+    plato = models.ForeignKey(Plato, on_delete=models.PROTECT)
+    nota = models.TextField(blank=True, null=True, verbose_name="Nota")
+
+    def __str__(self):
+        return f"{self.cantidad}x {self.plato.nombre}"
